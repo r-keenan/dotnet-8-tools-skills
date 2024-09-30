0c91f26639fdec82086bf0a34ab9afa258bebe6a
r-keenan <ross.keenan@protonmail.com>
2024-09-30 09:07:13 -0500

    added console app for working with reflection.

---
+++ Chapter07/WorkingWithReflection/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+
+Console.WriteLine("Assembly metadata:");
+Assembly? assembly = Assembly.GetEntryAssembly();
+
+if (assembly is null)
+{
+    Console.WriteLine("Failed to get entry assembly.");
+    return;
+}
+
+Console.WriteLine($"    Full name: {assembly.FullName}");
+Console.WriteLine($"    Location: {assembly.Location}");
+Console.WriteLine($"    Entry point: {assembly.EntryPoint?.Name}");
+
+IEnumerable<Attribute> attributes = assembly.GetCustomAttributes();
+Console.WriteLine($"    Assembly-level attributes:");
+foreach (Attribute a in attributes)
+{
+   Console.WriteLine($"     {a.GetType()}");
+}
