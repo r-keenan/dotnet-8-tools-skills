cf92e8dd2f8d0ee35daac9bf9c811ea62e65505c
r-keenan <ross.keenan@protonmail.com>
2024-10-18 08:32:27 -0500

    added load testing app.

---
+++ Chapter13/NBomberApp/Program.cs
@@ -0,0 +1,54 @@
+﻿using NBomber.Contracts;
+using NBomber.CSharp;
+using NBomber.Http;
+using NBomber.Http.CSharp;
+using NBomber.Plugins.Network.Ping;
+
+// Use System.Http.HttpClient to make HTTP requests.
+using HttpClient client = new();
+
+LoadSimulation[] loads =
+[
+    // Ramp up to 50 RPS during one minute
+    Simulation.RampingInject(
+        rate: 50,
+        interval: TimeSpan.FromSeconds(1),
+        during: TimeSpan.FromMinutes(1)
+    ),
+    // Maintain 50 RPS for another minute
+    Simulation.Inject(rate: 50, interval: TimeSpan.FromSeconds(1), during: TimeSpan.FromMinutes(1)),
+    // Ramp down to 0 RPS during one minute
+    Simulation.RampingInject(
+        rate: 0,
+        interval: TimeSpan.FromSeconds(1),
+        during: TimeSpan.FromMinutes(1)
+    ),
+];
+
+ScenarioProps scenario = Scenario
+    .Create(
+        name: "http_scenario",
+        run: async context =>
+        {
+            HttpRequestMessage request = Http.CreateRequest(
+                    "GET",
+                    "http://localhost:5255/weatherforecast"
+                )
+                .WithHeader("Accept", "application/json");
+
+            // Use WithHeader and WithBody to send a JSON payload
+            Response<HttpResponseMessage> response = await Http.Send(client, request);
+
+            return response;
+        }
+    )
+    .WithoutWarmUp()
+    .WithLoadSimulations(loads);
+
+NBomberRunner
+    .RegisterScenarios(scenario)
+    .WithWorkerPlugins(
+        new PingPlugin(PingPluginConfig.CreateDefault("nbomber.com")),
+        new HttpMetricsPlugin([HttpVersion.Version1])
+    )
+    .Run();
---
+++ Chapter13/Northwind.WebApiAot/Program.cs
@@ -0,0 +1,54 @@
+using System.Text.Json.Serialization;
+
+var builder = WebApplication.CreateSlimBuilder(args);
+
+// Add services to the container.
+builder.Services.ConfigureHttpJsonOptions(options => {
+    options.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default);
+})
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+var summaries = new[]
+{
+    "Freezing",
+    "Bracing",
+    "Chilly",
+    "Cool",
+    "Mild",
+    "Warm",
+    "Balmy",
+    "Hot",
+    "Sweltering",
+    "Scorching",
+};
+
+app.MapGet(
+    "/weatherforecast",
+    () =>
+    {
+        var forecast = Enumerable
+            .Range(1, 5)
+            .Select(index => new WeatherForecast(
+                DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                Random.Shared.Next(-20, 55),
+                summaries[Random.Shared.Next(summaries.Length)]
+            ))
+            .ToArray();
+        return forecast;
+    }
+);
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+
+[JsonSerializable(typeof(WeatherForecast[]))]
+internal partial class AppJsonSerializerContext: JsonSerializerContext{
+
+}
